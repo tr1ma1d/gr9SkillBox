248c56fc98ddfb42484b195702c36fd097fbed4f
BlackSmokingOfMoon <104624954+tr1ma1d@users.noreply.github.com>
2023-09-19 14:17:45 +0300

    Add files via upload

---
+++ GibddGroupNine/GibddGroupNine/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace GibddGroupNine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbLogin = new System.Windows.Forms.TextBox();
+            this.logButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.signButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbLogin
+            // 
+            this.tbLogin.Font = new System.Drawing.Font("Calibri", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.tbLogin.Location = new System.Drawing.Point(93, 70);
+            this.tbLogin.Name = "tbLogin";
+            this.tbLogin.Size = new System.Drawing.Size(332, 27);
+            this.tbLogin.TabIndex = 0;
+            // 
+            // logButton
+            // 
+            this.logButton.Font = new System.Drawing.Font("Calibri", 12.75F);
+            this.logButton.Location = new System.Drawing.Point(197, 184);
+            this.logButton.Name = "logButton";
+            this.logButton.Size = new System.Drawing.Size(98, 35);
+            this.logButton.TabIndex = 1;
+            this.logButton.Text = "Log in";
+            this.logButton.UseVisualStyleBackColor = true;
+            this.logButton.Click += new System.EventHandler(this.logButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(88, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 26);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "login";
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Font = new System.Drawing.Font("Calibri", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.tbPassword.Location = new System.Drawing.Point(93, 141);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(332, 27);
+            this.tbPassword.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(88, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 26);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "password";
+            // 
+            // signButton
+            // 
+            this.signButton.Font = new System.Drawing.Font("Calibri", 12.25F);
+            this.signButton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.signButton.Location = new System.Drawing.Point(335, 184);
+            this.signButton.Name = "signButton";
+            this.signButton.Size = new System.Drawing.Size(90, 35);
+            this.signButton.TabIndex = 6;
+            this.signButton.Text = "Sign In";
+            this.signButton.UseVisualStyleBackColor = true;
+            this.signButton.Click += new System.EventHandler(this.signButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 263);
+            this.Controls.Add(this.signButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.logButton);
+            this.Controls.Add(this.tbLogin);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbLogin;
+        private System.Windows.Forms.Button logButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button signButton;
+    }
+}
+
---
+++ GibddGroupNine/GibddGroupNine/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+namespace GibddGroupNine
+{
+    public partial class Form1 : Form
+    {
+        
+        public static string stringConnect = "server=localhost;userid=root;database=gybdd;password=123456;sslmode=none";
+       
+        public Form1()
+        {
+            InitializeComponent();
+         
+        }
+        private void LogIn()
+        {
+
+            MySqlConnection conn = new MySqlConnection(stringConnect);
+            conn.Open();
+            string login = tbLogin.Text;
+            string password = tbPassword.Text;
+            string sqlQuery = $"SELECT `login`, `password` FROM `identification` WHERE `login` = @uL AND `password` = @uP";
+
+            MySqlDataAdapter sqlAdapter = new MySqlDataAdapter(sqlQuery,conn);
+            MySqlCommand msCommand = new MySqlCommand(sqlQuery,conn);
+            
+            DataTable dt = new DataTable();
+            msCommand.Parameters.Add("@uL", MySqlDbType.VarChar).Value = login;
+            msCommand.Parameters.Add("@uP", MySqlDbType.VarChar).Value = password;
+
+            sqlAdapter.SelectCommand = msCommand;
+            sqlAdapter.Fill(dt);
+
+            if(dt.Rows.Count > 0)
+            {
+                MessageBox.Show("You`re successfull login");
+
+                Information inf = new Information();
+                inf.OnTransition += CloseCurrentForm;
+                inf.Show();
+               
+            }
+            else
+            {
+                MessageBox.Show("No");
+            }
+
+
+
+            conn.Close();
+        }
+        private void SignIn()
+        {
+            MySqlConnection conn = new MySqlConnection(stringConnect);
+            conn.Open();
+            string login = tbLogin.Text;
+            string password = tbPassword.Text;
+
+            string sqlQuery = $"INSERT INTO identification(login, password) VALUES ('{login}','{password}')";
+
+            MySqlDataAdapter myAdapter = new MySqlDataAdapter(sqlQuery, conn);
+            MySqlCommand mySqlCommand = new MySqlCommand(sqlQuery, conn);
+            DataTable dt = new DataTable();
+            myAdapter.Fill(dt);
+
+            conn.Close();
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void logButton_Click(object sender, EventArgs e)
+        {
+            LogIn();
+        }
+        //close form1
+        private void CloseCurrentForm()
+        {
+            this.Close();
+        }
+
+        private void signButton_Click(object sender, EventArgs e)
+        {
+            SignIn();
+        }
+       
+    }
+}
