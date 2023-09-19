d7925082c3348ce2725c6a5bd48aff94bc996e88
nikita <la3xmus@gmail.com>
2023-09-19 14:15:13 +0300

    carcas

---
+++ GibddGroupNine/Information.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GibddGroupNine
+{
+    public partial class Information : Form
+    {
+        public event Action OnTransition;
+        public Information()
+        {
+            InitializeComponent();
+        }
+
+        private void Information_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
